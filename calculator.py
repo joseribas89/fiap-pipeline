1399325da1a88731121d5d6993b4f5e3358cfe63
joseribas89 <joseribas89@hotmail.com>
2023-04-20 13:00:10 -0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,18 +1,25 @@
 import random
 
-def generatePassword(lengths):
+def generatePassword(numPasswords, minLength, maxLength):
+
     alphabet = "abcdefghijklmnopqrstuvwxyz"
+
     passwords = []
-    for length in lengths:
-        if length < 3:
-            length = 3
+
+    for i in range(numPasswords):
+
+        length = random.randint(minLength, maxLength)
+
         password = ""
         for j in range(length):
             next_letter_index = random.randrange(len(alphabet))
             password = password + alphabet[next_letter_index]
+
         password = replaceWithNumber(password)
         password = replaceWithUppercaseLetter(password)
+
         passwords.append(password)
+
     return passwords
 
 
