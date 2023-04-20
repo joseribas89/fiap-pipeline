0612468b9f5357a5d8174dd3941846f5961c6097
joseribas89 <joseribas89@hotmail.com>
2023-04-20 13:18:04 -0300

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -5,7 +5,7 @@ class TestCalculator(unittest.TestCase):
 
     def test_generate_password(self):
         passwords = generatePassword(10, 5, 15)
-        self.assertEqual(len(passwords), 3)
+        self.assertEqual(len(passwords), 3, 15)
         for password in passwords:
             self.assertGreaterEqual(len(password), 5)
             self.assertLessEqual(len(password), 15)
